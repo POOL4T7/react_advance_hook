f7e2da1baedc919c40009d3c93f41fbd271f5206
Gulshan gupta <abhinavg90834@gmail.com>
2023-06-06 23:32:00 +0530

    next ui added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Header from "./components/Header";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+// import Document, { Html, Head, Main, NextScript } from 'next/document';
+// import { CssBaseline } from '@nextui-org/react';
 
 function App() {
   return (
